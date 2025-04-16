18a2e238cd973f882a155c6d234e629f2cb1aba9
NitionVR <kmaganyane83@gmail.com>
2025-04-16 14:21:17 +0200

    Add a Todo Model

---
+++ todoapp/src/main/java/com/nition/todoapp/models/Todo.java
@@ -0,0 +1,37 @@
+package com.nition.todoapp.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String Title;
+
+    @Column
+    private String description;
+
+    @Column
+    private boolean completed;
+
+    @Column
+    private LocalDateTime createdAt = LocalDateTime.now();  
+}
